89aacc34636920cd6c251571e2ac27e53dcefd58
s0896253 <s0896253@d7f5ad51-1e5f-4e35-9a39-61d036ba1df1>
2012-06-08 14:20:23 +0000

    experiment with PTSW and Prefix.cc
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/infbaxtree/workspace/rdfasquare@213 d7f5ad51-1e5f-4e35-9a39-61d036ba1df1

--- src/experiment/Experiment.java
+++ src/experiment/Experiment.java
@@ -12,8 +12,48 @@ import RDFaAnnotator.main.VocabRDFaAnnotator;
 
 
 public class Experiment {
+	
+	public void experimentWithPrefixCC(){
+		File file = new File("all.file.txt");
+		try{
+			FileInputStream fis = new FileInputStream(file);
+			InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+			String url = "";
+			while((url = br.readLine()) != null){
+				try{
+					if(url.startsWith("#")){
+						continue;
+					}
+					url = url.split("\\t")[1];
+					if(url.equalsIgnoreCase("???")){
+						continue;
+					}
+					VocabRDFaAnnotator vrdfaa = new VocabRDFaAnnotator(url);
+					//long begin = System.currentTimeMillis();
+					System.out.println("<<<<"+url+">>>>");
+					vrdfaa.gernerateRDFa("complete");
+					System.out.println();
+					//long end = System.currentTimeMillis();
+					//System.out.println(end-begin+"ms");
+				}
+				catch(Exception fnfe){
+					continue;
+				}
+			}
+		}
+//		catch(FileNotFoundException fnfe){
+//			fnfe.printStackTrace();
+//		}
+		catch(UnsupportedEncodingException uee){
+			uee.printStackTrace();
+		}
+		catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+	}
 
-	public void experimentWithPublishingVocab(){
+	public void experimentWithPTSW(){
 		File file = new File("pingthesemanticweb.txt");
 		try{
 			FileInputStream fis = new FileInputStream(file);
@@ -49,6 +89,6 @@ public class Experiment {
 	
 	public static void main(String[] args){
 		Experiment exp = new Experiment();
-		exp.experimentWithPublishingVocab();
+		exp.experimentWithPrefixCC();
 	}
 }
